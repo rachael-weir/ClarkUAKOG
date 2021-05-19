a507647a23c8fe9003d132f95063cc23958017c2
bpartida <bpartida@clarku.edu>
2021-05-19 16:19:06 -0400

    server initialized

---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const express = require("express");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+//Add sessions
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+//Configure body-parser and set static dir path.
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+//Initialize passport
+app.use(session({
+    secret: process.env.PASSPORT_SECRET, // used to encrypt session key
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Configure Mongoose
+mongoose.connect('mongodb://localhost:27017/movieDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
+
+app.listen(3000, function () {
+    console.log("server started at 3000");
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
