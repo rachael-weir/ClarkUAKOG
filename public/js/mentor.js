028899d78f9a78a333429d24a7fcaccbf779fc94
rachael-weir <rweir@clarku.edu>
2021-05-21 21:21:19 -0400

    commit 05/21

--- public/js/mentor.js
+++ public/js/mentor.js
@@ -1,6 +1,5 @@
 $('.mentor').append("<div class='col-lg-9 mentor_col'></div>");
 
-$('.mentor_col').append("<img src='img/current_logo.PNG' class='akog_logo_mentor'/>")
-    .append("<p>AKOG mentors are undergraduate female-aligned people at Clark University. Interested in becoming a " +
+$('.mentor_col').append("<p>AKOG mentors are undergraduate female-aligned people at Clark University. Interested in becoming a " +
         "mentor? Email our Mentor Coordinators Deisy Rodriguez ([deirodriguez@clarku.edu]) or Sammy Eagen " +
         "([seagen@clarku.edu]) to learn about how to become involved!</p>");
